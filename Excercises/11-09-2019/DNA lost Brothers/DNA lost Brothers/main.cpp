04bbeb142c1f87729532e8bf40a3e059e62b0342
German David Guerrero Guerrero <gdguerrerog@unal.edu.co>
2019-09-12 23:46:21 -0500

    Lost brothers with less memory

--- Excercises/11-09-2019/DNA lost Brothers/DNA lost Brothers/main.cpp
+++ Excercises/11-09-2019/DNA lost Brothers/DNA lost Brothers/main.cpp
@@ -4,18 +4,22 @@ using namespace std;
 
 void solve(string DNA1, string DNA2){
     int n = DNA1.length();
-    int matrix[n + 1][n + 1];
+    int best[n + 1];
 
-    for(int i = 0, j; i < n + 1; i++){
-        for(j = 0; j < n + 1;j++) {
-            if(i == 0 || j == 0) matrix[i][j] = 0;
-            else if(DNA1[i - 1] == DNA2[j - 1]) matrix[i][j] = matrix[i - 1][j - 1] + 1;
-            else matrix[i][j] = max(matrix[i - 1][j], matrix[i][j - 1]);
+    for(int i = 0; i <= n; i++) best[i] = 0;
+
+    for(int i = 1, j; i <= n; i++){
+        for(j = 1; j <= n; j++) {
+            //cout << best[j] << "-";
+            if(DNA1[i - 1] == DNA2[j - 1]) best[j] = best[j - 1] + 1;
+            else if(best[j - 1] > best[j]) best[j] = best[j - 1];
+            //cout << best[j] << "\t";
         }
+        //cout << endl;
     }
 
-    //cout << (matrix[n][n]) << " " << (n*0.99) << " " <<  (matrix[n][n] >= n*0.99) << " " << n <<endl;
-    if(matrix[n][n]*1.0 >= n*0.99) cout << "Long lost brothers D:\n";
+    //cout << (best[n]) << " " << (n*0.99) << " " <<  (best[n] >= n*0.99) << " " << n <<endl;
+    if(best[n] >= n*0.99) cout << "Long lost brothers D:\n";
     else cout << "Not brothers :(\n";
 }
 
@@ -32,9 +36,9 @@ void testInput(){
         DNA2 += "A";
     }
     DNA1 += "A";
-    DNA2 += "B";
-    DNA1 += "A";
-    DNA2 += "B";
+    DNA2 += "C";
+    DNA1 += "C";
+    DNA2 += "A";
     solve(DNA1, DNA2);
 }
 int main()
