02e50074c66403a8b61ee43db4b9de6a5abd7d78
German David Guerrero Guerrero <gdguerrerog@unal.edu.co>
2019-09-13 01:48:43 -0500

    OTher fomr DNA brohers

--- Excercises/11-09-2019/DNA lost Brothers/DNA lost Brothers/main.cpp
+++ Excercises/11-09-2019/DNA lost Brothers/DNA lost Brothers/main.cpp
@@ -4,22 +4,33 @@ using namespace std;
 
 void solve(string DNA1, string DNA2){
     int n = DNA1.length();
-    int best[n + 1];
+    int best1[n + 1], best2[n + 1];
+    int * current, * past;
+    bool used1 = true;
 
-    for(int i = 0; i <= n; i++) best[i] = 0;
+    for(int i = 0; i <= n; i++) best1[i] = best2[i] = 0;
 
     for(int i = 1, j; i <= n; i++){
-        for(j = 1; j <= n; j++) {
-            //cout << best[j] << "-";
-            if(DNA1[i - 1] == DNA2[j - 1]) best[j] = best[j - 1] + 1;
-            else if(best[j - 1] > best[j]) best[j] = best[j - 1];
-            //cout << best[j] << "\t";
+        if(used1){
+            current = best2;
+            past = best1;
+        }else{
+            current = best1;
+            past = best2;
         }
+        for(j = 1; j <= n; j++) {
+
+            //cout << current[j - 1] << "-"<<past[j-1]<<"-";
+            if(DNA1[i - 1] == DNA2[j - 1]) current[j] = past[j - 1] + 1;
+            else current[j] = max(current[j - 1], past[j]);
+            //cout << current[j] << "-"<<past[j]<< "\t";
+        }
+        used1 = !used1;
         //cout << endl;
     }
 
-    //cout << (best[n]) << " " << (n*0.99) << " " <<  (best[n] >= n*0.99) << " " << n <<endl;
-    if(best[n] >= n*0.99) cout << "Long lost brothers D:\n";
+    //cout << (current[n]) << " " << (n*0.99) << " " <<  (current[n] >= n*0.99) << " " << n <<endl;
+    if(current[n] >= n*0.99) cout << "Long lost brothers D:\n";
     else cout << "Not brothers :(\n";
 }
 
