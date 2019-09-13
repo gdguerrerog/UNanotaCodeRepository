82b4f409eca48e5ed4cb76874d2c903b5dd97eaa
German David Guerrero Guerrero <gdguerrerog@unal.edu.co>
2019-09-13 02:53:40 -0500

    Most efficent solution to DNA Brothers

--- Excercises/11-09-2019/DNA lost Brothers/DNA lost Brothers/main.cpp
+++ Excercises/11-09-2019/DNA lost Brothers/DNA lost Brothers/main.cpp
@@ -1,36 +1,30 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 void solve(string DNA1, string DNA2){
     int n = DNA1.length();
     int best1[n + 1], best2[n + 1];
-    int * current, * past;
-    bool used1 = true;
+    int * current = best1, * past = best2, *tmp;
+    int minAcepted = ceil(n*0.99);
 
-    for(int i = 0; i <= n; i++) best1[i] = best2[i] = 0;
+    for(int i = 0; i <= n; i++) best2[i] = 0;
 
-    for(int i = 1, j; i <= n; i++){
-        if(used1){
-            current = best2;
-            past = best1;
-        }else{
-            current = best1;
-            past = best2;
-        }
+    int register j;
+    for(int i = 1; i <= n; i++){
         for(j = 1; j <= n; j++) {
-
-            //cout << current[j - 1] << "-"<<past[j-1]<<"-";
             if(DNA1[i - 1] == DNA2[j - 1]) current[j] = past[j - 1] + 1;
             else current[j] = max(current[j - 1], past[j]);
-            //cout << current[j] << "-"<<past[j]<< "\t";
         }
-        used1 = !used1;
-        //cout << endl;
-    }
 
-    //cout << (current[n]) << " " << (n*0.99) << " " <<  (current[n] >= n*0.99) << " " << n <<endl;
-    if(current[n] >= n*0.99) cout << "Long lost brothers D:\n";
+        if(n - i + 1 < minAcepted - current[n]) break; // No brothers
+
+        tmp = past;
+        past = current;
+        current = tmp;
+    }
+    if(current[n] >= minAcepted)  cout << "Long lost brothers D:\n";
     else cout << "Not brothers :(\n";
 }
 
@@ -41,20 +35,31 @@ void consoleInput(){
 }
 
 void testInput(){
+    //1e5 B: 53.875
+    //1e5 N: 8.160
+    //1e4 B: 0.562
+    //1e4 N: 0.098
     string DNA1 = "", DNA2 = "";
-    for(int i = 0; i < 98; i++){
+
+    for(int i = 0; i < 1e3; i++){
         DNA1 += "A";
         DNA2 += "A";
     }
-    DNA1 += "A";
-    DNA2 += "C";
-    DNA1 += "C";
-    DNA2 += "A";
+    //*
+    for(int i = 0; i < 1e4 - 1e3; i++){
+        DNA1 += "A";
+        DNA2 += "B";
+
+    }
+    //*/
+
+    //cout << "Starting solve\n";
     solve(DNA1, DNA2);
 }
 int main()
 {
     consoleInput();
     //testInput();
+
     return 0;
 }
