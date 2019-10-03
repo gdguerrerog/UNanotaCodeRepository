ce0083ca58f2946f346bfe4c5ecc6637db301944
German David Guerrero Guerrero <gdguerrerog@unal.edu.co>
2019-10-02 19:44:25 -0500

    :art: :zap: fibonacci exercice :ok_hand:

--- Excercises/Nacionales 2019/Fibcod Cryptocurrency/main.cpp
+++ Excercises/Nacionales 2019/Fibcod Cryptocurrency/main.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+#define MODULO 524288
 
 /*
 
@@ -10,11 +11,14 @@ matrix indexes are:
 2 3
 
 */
-void matrix2x2Mult(long long * m1, long long * m2, long long * exit){
-    exit[0] = m1[0]*m2[0] + m1[1]*m2[2];
-    exit[1] = m1[0]*m2[1] + m1[1]*m2[3];
-    exit[2] = m1[2]*m2[0] + m1[3]*m2[2];
-    exit[3] = m1[2]*m2[1] + m1[3]*m2[3];
+long long * matrix2x2Mult(long long * m1, long long * m2){
+    long long * exit = new long long[4];
+    exit[0] = (m1[0]*m2[0] + m1[1]*m2[2]) % MODULO;
+    exit[1] = (m1[0]*m2[1] + m1[1]*m2[3]) % MODULO;
+    exit[2] = (m1[2]*m2[0] + m1[3]*m2[2]) % MODULO;
+    exit[3] = (m1[2]*m2[1] + m1[3]*m2[3]) % MODULO;
+
+    return exit;
 }
 
 void copyMatrix(long long * origin, long long * destiny){
@@ -28,57 +32,67 @@ void printMatrix(long long * matrix){
     cout << matrix[0] << "\t" << matrix[1] << endl << matrix[2] << "\t" << matrix[3] << endl;
 }
 
-void fastExp(long long * matrix  , long long pow, long long * exit){
-    long long cPow = 1;
+long long * fastExp(long long * matrix, long long pow){
+    long long cPow = 0;
     int bitCounter = 1;
     long long * tmp = new long long[4];
-    long long * tmp2 = new long long[4];
     copyMatrix(matrix, tmp);
 
-    exit[0] = exit[3] = 1;
-    exit[1] = exit[2] = 0;
-    if((pow & 1) != 0){
-        matrix2x2Mult(exit, tmp, tmp2);
-        copyMatrix(tmp2, exit);
+    long long * exit = new long long[4]{1, 0, 0, 1};
+    if((pow & 1) != 0) {
+        exit = matrix2x2Mult(exit, tmp);
+        cPow = 1;
     }
 
     while (cPow < pow){
-        matrix2x2Mult(tmp, tmp, tmp2);
-        copyMatrix(tmp2, tmp);
-        if(((pow >> bitCounter) & 1) != 0){
-            matrix2x2Mult(exit, tmp, tmp2);
-            copyMatrix(tmp2, exit);
+        tmp = matrix2x2Mult(tmp, tmp);
+        if((pow & (1 << bitCounter)) != 0) {
+            exit = matrix2x2Mult(exit, tmp);
+            cPow = cPow | (1 << bitCounter);
         }
-        if(cPow == 0) cPow = 1;
-        else cPow = cPow << 1;
         bitCounter++;
-
-        /*cout << "--------------------------\n";
-        printMatrix(tmp);
-        cout << endl;
-        printMatrix(exit);
-        cout << endl;
-        cout << "pow: " << cPow << ", bit: " << bitCounter << endl;*/
-
     }
+
+    return exit;
 }
 
 int main()
 {
     int inputSize, cIndex;
-    long long currentPow;
-    long long * inputs, matrix;
+    long long  sum;
+    long long * inputs, * matrix, * tmpMatrix;
     while(true){
         cin >> inputSize;
         if(inputSize == 0) return 0;
         inputs = new long long[inputSize];
 
         for(int i = 0; i < inputSize; i++) cin >> inputs[i];
-        currentPow = cIndex = 0;
+        sum = 0;
+        cIndex = inputSize - 1;
         matrix = new long long[4]{1, 1, 1, 0};
-        if((inputSize & 1) == 0)
 
+        if((inputSize & 1) == 0){
+            cIndex--;
+            matrix = fastExp(new long long[4]{1, 1, 1, 0}, inputs[cIndex] + 1);
+        }
+
+        while(cIndex >= 0){
+            //printMatrix(matrix);
+            for(int i = 0; i < inputs[cIndex]; i++){
+                sum = (sum + matrix[0]) % MODULO;
+                matrix = matrix2x2Mult(matrix, new long long[4]{1, 1, 1, 0});
+                //cout << "Sum: " << sum << endl;
+            }
+            cIndex--;
+            if(cIndex > 0){
+                tmpMatrix = fastExp(new long long[4]{1, 1, 1, 0}, inputs[cIndex]);
+                matrix = matrix2x2Mult(matrix, tmpMatrix);
+            }
+            cIndex--;
+        }
+        cout << sum << "\n";
 
     }
+
     return 0;
 }
